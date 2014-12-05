6bd717da3a5b3240366d85431755a3983a818695
eva-xuyen <eva.xuyenhtk@gmail.com>
2014-12-05 16:55:45 +0700

    fix bash rule and logic

--- day04.sh
+++ day04.sh
@@ -1,57 +1,42 @@
 #!/bin/sh
 
 # Set variable
-URL="https://s3-ap-northeast-1.amazonaws.com/aucfan-tuningathon/dataset/"
-PREFIX="result.day"
-EXT_FILE=".log.gz"
-FILE_RESULT='result.log'
-
-### Create file result
-if [ ! -f $FILE_RESULT ]
-then
-	touch $FILE_RESULT
-else
-	> $FILE_RESULT
-fi
+url="https://s3-ap-northeast-1.amazonaws.com/aucfan-tuningathon/dataset/"
+prefix="result.day"
+extFile=".log.gz"
+combineFile=$(mktemp)
 
 ### Get each file
-for i in `seq -w 5 20`
+for i in `seq -w 1 20`
 do
+    fileNameZip=$prefix$i$extFile
+    linkFile=$url$fileNameZip
 
-	fileNameZip=$PREFIX$i$EXT_FILE
-	linkFile=$URL$fileNameZip
-			
-	### Download file
-	if [ ! -f $fileNameZip ]
-	then
-		wget --no-check-certificate $linkFile
-	fi
-	
-	### Get data user unique by day
-	totalUser=$(cut -f3 $fileNameZip | sort -u | wc -l)
-	echo "Unique user in day"$i" : "$totalUser >> $FILE_RESULT
+    ### Download file
+    if [ ! -f $fileNameZip ]
+    then
+        wget --no-check-certificate $linkFile
+    fi
 
+    ### Combine fine
+    zcat $fileNameZip >> $combineFile
+
+    ### Get data user unique by day
+    totalUser=$(zcat $fileNameZip | cut -f3 | sort -u | wc -l)
+    echo "Unique user in day"$i" : "$totalUser
 done
-
-### Combine fine
-combineFile='out.log'
-if [ ! -f "$combineFile" ]
-then
-	zcat $PREFIX*$EXT_FILE > $combineFile
-fi
 
 ### Count total user
 totalUser=$(cut -f3 $combineFile | sort -u | wc -l)
-echo "Total unique user in all day : "$totalUser >> $FILE_RESULT
-
+echo "Total unique user in all day : "$totalUser
 
 ### Get list region
-region=$(cut -f2 $combineFile | sort | uniq)
+region=$(cut -f2 $combineFile | sort -u)
 region=(`echo $region | tr " " "\n"`)
 
 ### Get unique number by region
 for i in "${region[@]}"
 do
    count=$(grep "$i" $combineFile | cut -f3 | sort -u | wc -l)
-   echo "Total unique user of region "$i" : "$count >> $FILE_RESULT
+   echo "Total unique user of region "$i" : "$count
 done
