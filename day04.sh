fc3b64236049d67f1d47d1f9be909a2b9658ae2d
eva-xuyen <eva.xuyenhtk@gmail.com>
2014-12-29 10:12:04 +0700

    refs #4226 @1h fix code bash

--- day04.sh
+++ day04.sh
@@ -6,34 +6,34 @@ prefix="result.day"
 extFile=".log.gz"
 combineFile=$(mktemp)
 
-### Get each file
+### Download file
 for i in `seq -w 1 20`
 do
     fileNameZip=$prefix$i$extFile
     linkFile=$url$fileNameZip
-
     ### Download file
     if [ ! -f $fileNameZip ]
     then
         wget --no-check-certificate $linkFile
     fi
+done
 
-    ### Combine fine
-    zcat $fileNameZip >> $combineFile
-
-    ### Get data user unique by day
-    totalUser=$(zcat $fileNameZip | cut -f3 | sort -u | wc -l)
+### Get data user unique by day
+for i in `seq -w 1 20`
+do
+    totalUser=$(zcat $prefix$i$extFile | cut -f3 | sort -u | wc -l)
     echo "Unique user in day"$i" : "$totalUser
 done
 
+############# Get data for region
+### Combine fine
+zcat ${prefix}*${extFile} > $combineFile
 ### Count total user
 totalUser=$(cut -f3 $combineFile | sort -u | wc -l)
 echo "Total unique user in all day : "$totalUser
-
 ### Get list region
 region=$(cut -f2 $combineFile | sort -u)
 region=(`echo $region | tr " " "\n"`)
-
 ### Get unique number by region
 for i in "${region[@]}"
 do
